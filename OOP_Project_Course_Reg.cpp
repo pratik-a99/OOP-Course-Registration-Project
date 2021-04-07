accda0289683e7c4514c39860bf2110f2bd691fe
Pratik Acharya <pratik300399@gmail.com>
2021-04-07 12:38:00 +0530

    Update OOP_Project_Course_Reg.cpp

--- OOP_Project_Course_Reg.cpp
+++ OOP_Project_Course_Reg.cpp
@@ -26,41 +26,41 @@ public:
     vector<string> Courses;         // Vector to store the courses
     vector<string> Section;         // Vector to store the sections
     
-    void Add_ENPM660(string section){
-        Courses.push_back("ENPM660");   
-        Section.push_back(section);
+    void Add_ENPM660(string section){           // Method to add course ENPM660
+        Courses.push_back("ENPM660");           // Add respective course to registered list
+        Section.push_back(section);             // Add respective section to registered list
         cout << endl << "Course ENPM660 for section " << section << " has been added" << endl;
     }
-    void Remove_ENPM660(string section){
-        Courses.erase(find(Courses.begin(),Courses.end(),"ENPM660"));
-        Section.erase(find(Section.begin(),Section.end(),section));
+    void Remove_ENPM660(string section){        // Method to remove course ENPM660
+        Courses.erase(find(Courses.begin(),Courses.end(),"ENPM660"));   // Remove respective course to registered list
+        Section.erase(find(Section.begin(),Section.end(),section));     // Remove respective section to registered list
         cout << endl << "Course ENPM660 has been removed" << endl;
     }
 
-    void Add_ENPM662(string section){
+    void Add_ENPM662(string section){           // Method to add course ENPM662
         Courses.push_back("ENPM662");
         Section.push_back(section);
         cout << endl << "Course ENPM662 for section " << section << " has been added" << endl;
     }
-    void Remove_ENPM662(string section){
+    void Remove_ENPM662(string section){        // Method to remove course ENPM662
         Courses.erase(find(Courses.begin(),Courses.end(),"ENPM662"));
         Section.erase(find(Section.begin(),Section.end(),section));
         cout << endl << "Course ENPM662 has been removed" << endl;
     }
 
-    void Add_ENPM809Y(string section){
+    void Add_ENPM809Y(string section){          // Method to add course ENPM809Y
         Courses.push_back("ENPM809Y");
         Section.push_back(section);
         cout << endl << "Course ENPM809Y for section " << section << " has been added" << endl;
     }
-    void Remove_ENPM809Y(string section){
+    void Remove_ENPM809Y(string section){       // Method to remove course ENPM809Y
         Courses.erase(find(Courses.begin(),Courses.end(),"ENPM809Y"));
         Section.erase(find(Section.begin(),Section.end(),section));
         cout << endl << "Course ENPM809Y has been removed" << endl;
     }
 
-    void Add_ENPM808X(string section, string permission){
-        if (permission == "ENPM809Y" || permission == "Yes"){
+    void Add_ENPM808X(string section, string permission){       // Method to add course ENPM808X
+        if (permission == "ENPM809Y" || permission == "Yes"){   // Condition to check if prerequisite conditions are met
             Courses.push_back("ENPM808X");
             Section.push_back(section);
             cout << endl << "Course ENPM808X for section " << section << " has been added" << endl;
@@ -68,7 +68,7 @@ public:
         else
             cout << endl << "You cannot register for ENPM808X" << endl;
     }
-    void Remove_ENPM808X(string section){
+    void Remove_ENPM808X(string section){                       // Method to remove course ENPM808X
         Courses.erase(find(Courses.begin(),Courses.end(),"ENPM808X"));
         Section.erase(find(Section.begin(),Section.end(),section));
         cout << endl << "Course ENPM808X has been removed" << endl;
@@ -78,16 +78,17 @@ public:
         cout << endl << "Name : " << Name << endl;
         cout << "Program : " << Program << endl;
         cout << "UID : " << UID << endl;
-        cout << "-----------------------" << endl;
-        cout << "Course     Section" << endl;
-        cout << "-----------------------" << endl;
+        cout << "-------------------------" << endl;
+        cout << "Course\t\tSection" << endl;
+        cout << "-------------------------" << endl;
         for(int i=0; i < Courses.size(); i++)
-            std::cout << Courses.at(i) << "     " << Section.at(i) << endl;
+            std::cout << Courses.at(i) << "  \t  " << Section.at(i) << endl;
+        cout << endl;
     }
 };
 
 int main(){
-    CourseRegistration Pratik = CourseRegistration("Pratik Acharya", "PMRO", 117513615);
+    CourseRegistration Pratik = CourseRegistration("Pratik Acharya", "PMRO", 117513615); // Creating Object for class CourseRegistration
     Pratik.Add_ENPM660("0101");
     Pratik.Add_ENPM662("0101");
     Pratik.Add_ENPM809Y("0101");
